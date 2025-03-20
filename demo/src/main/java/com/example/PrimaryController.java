b5cf28797cc5d229db63d8c8eabebb819863cb4b
Jesus <jesusgonzalvezgarcia2005@gmail.com>
2025-03-20 09:31:31 +0100

    primer commit

---
+++ demo/src/main/java/com/example/PrimaryController.java
@@ -0,0 +1,17 @@
+package com.example;
+
+import java.io.IOException;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextArea;
+import javafx.scene.text.Text;
+
+public class PrimaryController {
+
+    @FXML
+    TextArea tablaBSD;
+
+    @FXML
+    private void VerTodo() throws IOException {
+        
+    }
+}
